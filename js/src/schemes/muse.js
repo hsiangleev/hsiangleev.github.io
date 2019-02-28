2926ce338aa82386cdbffefd2f9c260838b8cd00
hsiangleev <1547600955@qq.com>
2019-02-28 13:21:38 +0800

    Site updated: 2019-02-28 13:21:28

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Tue Feb 26 2019 13:37:47 GMT+0800 (中国标准时间)
+// build time:Thu Feb 28 2019 13:21:27 GMT+0800 (中国标准时间)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
