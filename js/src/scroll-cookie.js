a2868e408b01bc26f85e4b8b0ccae619d91bc54b
hsiangleev <1547600955@qq.com>
2019-03-09 10:56:34 +0800

    Site updated: 2019-03-09 10:56:22

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 07 2019 11:18:42 GMT+0800 (中国标准时间)
+// build time:Sat Mar 09 2019 10:56:20 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
