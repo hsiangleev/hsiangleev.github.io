90c9c93cee200a443d9567af48b4a30b0988d959
hsiangleev <1547600955@qq.com>
2019-08-20 17:46:21 +0800

    Site updated: 2019-08-20 17:46:07

--- js/src/bg.js
+++ js/src/bg.js
@@ -1,3 +1,3 @@
-// build time:Wed Aug 07 2019 17:38:29 GMT+0800 (GMT+08:00)
+// build time:Tue Aug 20 2019 17:46:06 GMT+0800 (GMT+08:00)
 (function(){var t={1:"https://uploadbeta.com/api/pictures/random",2:"https://uploadbeta.com/api/pictures/random/?key=BingEverydayWallpaperPicture",3:"http://acg.bakayun.cn/randbg.php",4:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",11:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302&device=mobile"};var e=t["4"];if($(document.body).width()<768)e=t["11"];$(document.body).append('<div class="lee-bg">'+'<img src="'+e+'" />'+"</div>");$.get("https://v1.hitokoto.cn/",{},function(t){$(".site-description.motion-element").text(t.hitokoto)})})($);
 //rebuild by neat 
