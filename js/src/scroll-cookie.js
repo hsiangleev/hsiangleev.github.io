3685100d645f306ff639626c6aacd7477bbd1c7f
hsiangleev <1547600955@qq.com>
2020-05-19 11:29:20 +0800

    Site updated: 2020-05-19 11:29:02

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 29 2020 09:23:39 GMT+0800 (GMT+08:00)
+// build time:Tue May 19 2020 11:22:44 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
