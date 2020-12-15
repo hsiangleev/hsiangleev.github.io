897c9682c18e9b73c5917a75813ddc611b553cdc
hsiangleev <1547600955@qq.com>
2020-12-15 14:54:24 +0800

    Site updated: 2020-12-15 14:54:05

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Thu Dec 10 2020 10:34:06 GMT+0800 (GMT+08:00)
+// build time:Tue Dec 15 2020 14:54:02 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
