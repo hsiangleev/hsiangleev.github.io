7b9569f90f4d4443de647ce611c6e68dbb48b77d
hsiangleev <1547600955@qq.com>
2020-12-16 10:29:45 +0800

    Site updated: 2020-12-16 10:29:19

--- js/src/bg.js
+++ js/src/bg.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 15 2020 14:54:01 GMT+0800 (GMT+08:00)
+// build time:Wed Dec 16 2020 10:29:15 GMT+0800 (GMT+08:00)
 (function(){var t={1:"https://uploadbeta.com/api/pictures/random",2:"https://uploadbeta.com/api/pictures/random/?key=BingEverydayWallpaperPicture",3:"http://acg.bakayun.cn/randbg.php",4:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",11:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302&device=mobile"};var e=t["4"];if($(document.body).width()<768)e=t["11"];$(document.body).append('<div class="lee-bg">'+'<img src="'+e+'" />'+"</div>");$.get("https://v1.hitokoto.cn/",{},function(t){$(".site-description.motion-element").text(t.hitokoto)})})($);
 //rebuild by neat 
--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 15 2020 14:54:02 GMT+0800 (GMT+08:00)
+// build time:Wed Dec 16 2020 10:29:15 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
